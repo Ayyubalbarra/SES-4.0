f7346a6a755b3755fff00fcca41ec4f1a0a8d422
fawwaz2828 <fawwazwibowo96@gmail.com>
2024-12-19 02:43:48 +0700

    perbaikin sistem login

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@ session_unset(); // Menghapus semua sesi
 session_destroy(); // Menghancurkan sesi
 
 // Redirect ke halaman login atau landing page
-header("Location: ../index.php");
+header("Location: index.php");
 exit();
 ?>
