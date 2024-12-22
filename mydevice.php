03649347e9cad58a9327b0f9812ee80bb0baed29
fawwaz2828 <fawwazwibowo96@gmail.com>
2024-12-22 17:02:29 +0700

    dashboard

--- mydevice.php
+++ mydevice.php
@@ -23,6 +23,9 @@ include('includes/db.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="assets/css/dashboard.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 </head>
 <body>
 
